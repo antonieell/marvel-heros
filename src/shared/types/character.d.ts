00a9e1d61c8115299ab81a2de8b56c14a03bba1f
Antoniel Magalhães <antoniel2210@gmail.com>
2020-12-28 12:45:52 -0300

    chore: mock and type characters endpoint

---
+++ src/shared/types/character.d.ts
@@ -0,0 +1,74 @@
+type Series = {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: Array<{
+    resourceURI: string;
+    name: string;
+  }>;
+};
+
+type Events = {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: Array<{
+    resourceURI: string;
+    name: string;
+  }>;
+};
+type Stories = {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: Array<{
+    resourceURI: string;
+    name: string;
+    type: string;
+  }>;
+};
+type Comics = {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: Array<{
+    resourceURI: string;
+    name: string;
+  }>;
+};
+type Thumbnail = {
+  path: string;
+  extension: string;
+};
+export default interface ICharacter {
+  code: number;
+  status: string;
+  copyright: string;
+  attributionText: string;
+  attributionHTML: string;
+  data: {
+    offset: number;
+    limit: number;
+    total: number;
+    count: number;
+    results: Array<{
+      id: number;
+      name: string;
+      description: string;
+      modified: string;
+      resourceURI: string;
+      urls: Array<{
+        type: string;
+        url: string;
+      }>;
+      thumbnail: thumbnail;
+      comics: Comics;
+      stories: Stories;
+      events: Events;
+      series: Series;
+    }>;
+  };
+  etag: string;
+}
+
+
--- src/shared/types/index.d.ts
+++ src/shared/types/index.d.ts
@@ -1,1 +1,1 @@
-export {}
+export {default as ICharacter}from './character' 
