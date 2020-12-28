8ae595b88af6428b671924d326408b2804d12a30
Antoniel Magalhães <antoniel2210@gmail.com>
2020-12-28 19:07:10 -0300

    feat: create HeroDetails Component

--- src/pages/CharacterDetails.tsx
+++ src/pages/CharacterDetails.tsx
@@ -1,7 +1,38 @@
-import {Layout} from "../layout"
+import { Layout } from "../layout";
 
 export const CharacterDetails = () => {
   return (
-    <Layout>Charackers details</Layout>
-  )
-}
+    <Layout>
+      <Container>
+        <HeroDetails />
+        <SliderHeroContent />
+      </Container>
+    </Layout>
+  );
+};
+
+const Container: React.FC = ({ children }) => {
+  return (
+    <main className="flex flex-col justify-center w-full px-12 py-6 mx-auto">
+      {children}
+    </main>
+  );
+};
+const HeroDetails = () => {
+  return (
+    <section className="flex flex-col items-center justify-center md:flex-row gap-12 md:gap-16">
+      <div className="w-full h-64 max-w-lg bg-gray-300 rounded-lg ">
+        {/*<img src="" alt"/">*/}
+      </div>
+      <article>
+        <h3 className="font-extrabold text-red-600">Name</h3>
+        <p>""""""Name""""""</p>
+        <h3 className="font-extrabold text-red-600">Description</h3>
+        <p>""""""Description""""""</p>
+      </article>
+    </section>
+  );
+};
+const SliderHeroContent = () => {
+  return <section>SliderHeroContent</section>;
+};
