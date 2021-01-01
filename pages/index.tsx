a39935f94669b3f977df1ce7c8bfa24906fc97dd
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-01 00:59:25 -0300

    feat: change page Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import { CharactersList, Layout } from "components/index";
+import Head from "next/head";
 
 export default function CharactersListPage() {
   return (
     <Layout>
+      <Head>
+        <title>Marvel Heros</title>
+      </Head>
       <CharactersList />
     </Layout>
   );
