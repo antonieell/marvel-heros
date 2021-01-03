2f03af8c7c10f13ec36c5a900670225e6d7c5ad8
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-03 01:11:15 -0300

    fix: removing ; from the bottom of the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
